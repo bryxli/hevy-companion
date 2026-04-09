3ea3129d7a42fc066ddd7d8a3099528cd002811c
bryxli <bryanzero765@gmail.com>
2026-04-08 22:55:52 -0500

    Initial commit

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import globals from "globals";
+
+export default tseslint.config(
+  { ignores: ["dist", ".sst", "node_modules", "packages/frontend/**"] },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ["*.{ts,js}"],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+      globals: {
+        ...globals.node,
+      },
+      parserOptions: {
+        projectService: {
+          allowDefaultProject: ["eslint.config.js"],
+        },
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn",
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+      "@typescript-eslint/triple-slash-reference": "off",
+    },
+  },
+);
