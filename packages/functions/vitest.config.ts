16c770876e247f8e5f7f0425ad3daf92845ab38d
bryxli <bryanzero765@gmail.com>
2026-04-10 21:23:20 -0500

    Sonar

--- packages/functions/vitest.config.ts
+++ packages/functions/vitest.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   test: {
     coverage: {
       provider: "v8",
-      reporter: ["text", "html"],
+      reporter: ["text", "html", "lcov"],
       include: ["src/**/*.{ts,js}"],
       exclude: [],
     },
--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   test: {
     coverage: {
       provider: "v8",
-      reporter: ["text", "html"],
+      reporter: ["text", "html", "lcov"],
       include: ["packages/*/src/**/*.{ts,tsx,js,jsx}"],
       exclude: ["packages/core/src/index.ts", "packages/core/src/schemas.ts"],
     },
